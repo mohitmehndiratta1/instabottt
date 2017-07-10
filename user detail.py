c27e5353419d6088a4033e74f76135cd9c91d898
mohitmehndiratta <mohit.esteem@gmail.com>
2017-07-10 22:41:01 +0530

    final project

--- user detail.py
+++
@@ -1,9 +0,0 @@
-# Class to store user details
-
-class User:
-    def __init__(self,name,id):
-        self.name = name
-        self.id = id
-# List to store multiple object of class User
-
-user_list = []
